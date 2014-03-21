1c4c5e8ea7c0e453d0e80c1c0f9f9f3d26341713
Nevin Liber <nevin@cplusplusguy.com>
2014-03-21 14:24:19 -0500

    Moved everything into a.cpp

--- Main.cpp
+++
@@ -1,6 +0,0 @@
-extern int Main(int argc, char const*const argv[]);
-int Main(int argc, char const*const argv[])
-{
-    return 0;
-}
-
--- a.cpp
+++ a.cpp
@@ -1,4 +1,83 @@
+#include <algorithm>
+#include <fstream>
+#include <list>
+#include <regex>
+#include <tuple>
+#include <array>
+#include <functional>
+#include <locale>
+#include <scoped_allocator>
+#include <type_traits>
+#include <atomic>
+#include <future>
+#include <map>
+#include <set>
+#include <typeindex>
+#include <bitset>
+#include <initializer_list>
+#include <memory>
+#include <sstream>
+#include <typeinfo>
+#include <chrono>
+#include <iomanip>
+#include <mutex>
+#include <stack>
+#include <unordered_map>
+#include <codecvt>
+#include <ios>
+#include <new>
+#include <stdexcept>
+#include <unordered_set>
+#include <complex>
+#include <iosfwd>
+#include <numeric>
+#include <streambuf>
+#include <utility>
+#include <condition_variable>
 #include <iostream>
+#include <ostream>
+#include <string>
+#include <valarray>
+#include <deque>
+#include <istream>
+#include <queue>
+#include <strstream>
+#include <vector>
+#include <exception>
+#include <iterator>
+#include <random>
+#include <system_error>
+#include <forward_list>
+#include <limits>
+#include <ratio>
+#include <thread>
+
+#include <cassert>
+#include <cinttypes>
+#include <csignal>
+#include <cstdio>
+#include <cwchar>
+#include <ccomplex>
+#include <ciso646>
+//#include <cstdalign>
+#include <cstdlib>
+#include <cwctype>
+#include <cctype>
+#include <climits>
+#include <cstdarg>
+#include <cstring>
+#include <cerrno>
+#include <clocale>
+#include <cstdbool>
+#include <ctgmath>
+#include <cfenv>
+#include <cmath>
+#include <cstddef>
+#include <ctime>
+#include <cfloat>
+#include <csetjmp>
+#include <cstdint>
+//#include <cuchar>
 
 extern int Main(int argc, char const*const argv[]);
 
@@ -34,3 +113,9 @@ int main(int argc, char* argv[])
         return 255;
     }
 }
+
+int Main(int /* argc */, char const*const /* argv */[])
+{
+    return 0;
+}
+
