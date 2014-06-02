e2ed268fa0fe69cec95dd42afef26896e28858de
Nevin Liber <nevin@cplusplusguy.com>
2014-06-02 13:42:14 -0500

    Config script for setting up both clang and gcc

---
+++ silly.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -xv
+
+for d in "${@}"
+do
+    (
+        mkdir -p "${d}"/gcc "${d}"/clang
+        cp a.cpp CMakeLists.txt "${d}"
+
+        (
+            cd "${d}"/gcc
+            CC=/opt/local/bin/gcc-mp-4.9
+            CXX=/opt/local/bin/g++-mp-4.9
+            cmake ..
+        )
+
+        (
+            cd "${d}"/clang
+            CC=/opt/local/bin/clang-mp-4.9
+            CXX=/opt/local/bin/clang++-mp-4.9
+            cmake ..
+        )
+    )
+done
+
