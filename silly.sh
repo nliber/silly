d127535ae419597fc159b4cbec87d195e2d12476
Nevin Liber <nevin@cplusplusguy.com>
2014-09-30 14:21:51 -0500

    Fixed silly.sh to correctly build gcc5 and clang-3.6

--- silly.sh
+++ silly.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 set -xv
 
+gnu_version="${GNU_VERSION:-5}"
+clang_version="${CLANG_VERSION:-3.6}"
+
 for d in "${@}"
-do
-    (
-        mkdir -p "${d}"/gcc "${d}"/clang
-        cp a.cpp CMakeLists.txt "${d}"
+do (
+    mkdir -p "${d}"
+    cp a.cpp CMakeLists.txt "${d}"
+    cd "${d}"
 
-        (
-            cd "${d}"/gcc
-            CC=/opt/local/bin/gcc-mp-4.9
-            CXX=/opt/local/bin/g++-mp-4.9
-            cmake ..
-            make
-        )
+    for g in ${gnu_version}
+    do (
+        mkdir -p "gcc${g}"
+        cd "gcc${g}"
+        CC="/opt/local/bin/gcc-mp-${g}" CXX="/opt/local/bin/g++-mp-${g}" cmake ..
+        make
+    ) done
 
-        (
-            cd "${d}"/clang
-            CC=/opt/local/bin/clang-mp-4.9
-            CXX=/opt/local/bin/clang++-mp-4.9
-            cmake ..
-            make
-        )
-    )
-done
+    for c in ${clang_version}
+    do (
+        mkdir -p "clang${c}"
+        cd "clang${c}"
+        CC="/opt/local/bin/clang-mp-${c}" CXX="/opt/local/bin/clang++-mp-${c}" cmake ..
+        make
+    ) done
+) done
 
