45297ee2c0447cc3a68cd83ab62f7a43f97f9694
Nevin Liber <nevin@cplusplusguy.com>
2015-11-04 11:13:58 -0600

    script now inits and commits the first version to git

--- silly.sh
+++ silly.sh
@@ -13,6 +13,7 @@ do (
     for g in ${gnu_version}
     do (
         mkdir -p "gcc${g}"
+        echo "gcc${g}/" >> ".gitignore"
         cd "gcc${g}"
         CC="/usr/local/bin/gcc-${g}" CXX="/usr/local/bin/g++-${g}" cmake ..
         make
@@ -21,9 +22,15 @@ do (
     for c in ""
     do (
         mkdir -p "clang${c}"
+        echo "clang${c}/" >> ".gitignore"
         cd "clang${c}"
         CC="/usr/bin/clang${c}" CXX="/usr/bin/clang++${c}" cmake ..
         make
     ) done
+
+    git init
+    git add --verbose .
+    git commit --verbose --allow-empty-message --no-edit
+
 ) done
 
