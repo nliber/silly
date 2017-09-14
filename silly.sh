cf6ce6351a6bf6c665ec5977b05d18421f521594
Nevin Liber <nevin@cplusplusguy.com>
2017-09-14 16:33:35 -0500

    Adding support for YouCompleteMe and eclipse

--- silly.sh
+++ silly.sh
@@ -4,6 +4,26 @@ set -xv
 gnu_version="${GNU_VERSION:-"7.2 7.1"}"
 clang_version="${CLANG_VERSION:-"5.0 xcode"}"
 
+make="${MAKE:-"make"}"
+shopt -s nocasematch
+case "${make}" in
+    *ninja*)
+        generator_name="${GENERATOR_NAME:-"Eclipse CDT4 - Ninja"}"
+        ;;
+    *make*)
+        generator_name="${GENERATOR_NAME:-"Eclipse CDT4 - Unix Makefiles"}"
+        ;;
+esac
+
+cmake_options="-D CMAKE_EXPORT_COMPILE_COMMANDS=ON"
+
+eclipse_product_version="$(fgrep -m 1 -s "version=" "${HOME}/eclipse/cpp-oxygen/Eclipse.app/Contents/Eclipse/.eclipseproduct")"
+eclipse_product_version="${eclipse_product_version#"version="}"
+if [[ -n "${eclipse_product_version}" ]]
+then
+    cmake_options="${cmake_options} -D CMAKE_ECLIPSE_VERSION=${eclipse_product_version}"
+fi
+
 files="a.cpp main.cpp CMakeLists.txt"
 
 for d in "${@}"
@@ -18,8 +38,8 @@ do (
         mkdir -p "gcc${g}"
         echo "gcc${g}/" >> ".gitignore"
         cd "gcc${g}"
-        CC="/usr/local/gcc-${g}/bin/gcc-${g}" CXX="/usr/local/gcc-${g}/bin/g++-${g}" cmake ..
-        make
+        CC="/usr/local/gcc-${g}/bin/gcc-${g}" CXX="/usr/local/gcc-${g}/bin/g++-${g}" cmake ${cmake_options} -G "${generator_name}" ..
+        ${make} -j 2
     ) done
 
     for c in ${clang_version}
@@ -27,8 +47,8 @@ do (
         mkdir -p "clang${c}"
         echo "clang${c}/" >> ".gitignore"
         cd "clang${c}"
-        CC="/usr/local/bin/clang-${c}" CXX="/usr/local/bin/clang++-${c}" cmake ..
-        make
+        CC="/usr/local/bin/clang-${c}" CXX="/usr/local/bin/clang++-${c}" cmake ${cmake_options} -G "${generator_name}" ..
+        ${make} -j 2
     ) done
 
     git init
