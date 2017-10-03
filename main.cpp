468f8c25a66262de63dc95c3b471869a18d953b4
Nevin Liber <nevin@cplusplusguy.com>
2017-10-03 13:45:19 -0500

    moved Stopwatch to cool

--- main.cpp
+++ main.cpp
@@ -3,34 +3,12 @@ extern int Main(int argc, char const*const argv[]);
 #include <boost/exception/diagnostic_information.hpp>
 #include <cool/Args.h>
 #include <cool/chrono.h>
-#include <chrono>
+#include <cool/Stopwatch.h>
 #include <iostream>
-
-namespace {
-
-    class Stopwatch
-    {
-        using clock = std::chrono::high_resolution_clock;
-        using time_point = clock::time_point;
-
-        static time_point now() { return clock::now(); }
-
-    public:
-        using nanoseconds = std::chrono::nanoseconds;
-
-        nanoseconds lap() const noexcept
-        { return std::chrono::duration_cast<nanoseconds>(now() - start); }
-
-    private:
-        time_point start = now();
-    };
-
-} // namespace
 
 int main(int argc, char* argv[])
 {
     using cool::chrono::system_clock::operator<<;
-    using cool::chrono::duration::operator<<;
 
     std::cerr
         << "Built " <<  __FILE__ << " on " << __DATE__ << ' ' << __TIME__
@@ -38,7 +16,7 @@ int main(int argc, char* argv[])
         << " at " << std::chrono::system_clock{}
         << std::endl;
 
-    Stopwatch sw;
+    cool::Stopwatch<> sw;
     int status;
 	try { status = Main(argc, argv); }
     catch (...)
@@ -52,7 +30,7 @@ int main(int argc, char* argv[])
     std::cerr
         << "Stopped " << cool::Args(argv)
         << " at " << std::chrono::system_clock{}
-        << " (" << sw.lap()
+        << " (" << sw
         << ") with status=" << status
         << std::endl;
 
